8ebfef59e39d8154a973dcb5be11c2bc0191b2fb
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-02-01 12:41:27 -0800

    Update patch.js

--- patch.js
+++ patch.js
@@ -1,3 +1,4 @@
+
 function chelseyPatch(x, y) {
   noFill();
   stroke(238);
@@ -7,21 +8,29 @@ function chelseyPatch(x, y) {
  	fill(0,0,0);
   ellipse(x+150, y+150, 300, 300);
   
-  var radius=25;
+  var radius=0;
   	while (radius < 300) {
-       noFill();
- 			 stroke(150,100,255);
+      colorMode(HSB); 
+      noFill();
+ 			 stroke(210,75,200);
  			 strokeWeight(2);
  			 ellipse(x+150, y+150, radius);
       radius = radius + 25;
     }
   
-  noFill();  
-  stroke(60,0,75);
-  strokeWeight(10);
-  arc(x+150,y+150,300);
+ var radius2 = 270;
+  while (radius2 > 0) {
+    noFill();
+    stroke(0);
+    strokeWeight(10);
+    ellipse (x+150, y+150, radius2);
+    radius2 = radius2-20;
+  }
   	
 }
+
+background(255);
+chelseyPatch(0, 0); // draw patch at upper-left
 
 background(255);
 chelseyPatch(width-301, height-301); // draw patch at lower-right
