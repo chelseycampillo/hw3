8eb4be40573d9e59d11e7c92d26123cbd89f0cb9
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-02-01 18:38:18 -0800

    Create loops7.js

---
+++ loops7.js
@@ -0,0 +1,18 @@
+colorMode(HSB);
+background(255);
+
+function pattern(startingXlocation) {
+  var numberOfSquares = 0 //this is referring to the square within squre pattern
+	while (numberOfSquares < 32) {
+  			noFill();
+  	 		rect(startingXlocation+numberOfSquares, 170+numberOfSquares,62-(2*numberOfSquares), 62-(2*numberOfSquares));
+   			numberOfSquares = numberOfSquares + 8;
+    }
+	}
+  
+  
+pattern(0);
+pattern(68);
+pattern(136);
+pattern(204);
+pattern(272);
