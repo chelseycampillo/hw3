0a7ddd02ae3748bc06c8a720b6e781c6b5d8a964
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-02-02 11:06:11 -0800

    Update and rename loops7.js to art-deco.js

--- art-deco.js
+++ art-deco.js
@@ -2,7 +2,7 @@ colorMode(HSB);
 background(255);
 
 function pattern(startingXlocation) {
-  var numberOfSquares = 0 //this is referring to the square within squre pattern
+  var numberOfSquares = 0 //this is referring to the square within square pattern
 	while (numberOfSquares < 32) {
   			noFill();
   	 		rect(startingXlocation+numberOfSquares, 170+numberOfSquares,62-(2*numberOfSquares), 62-(2*numberOfSquares));
