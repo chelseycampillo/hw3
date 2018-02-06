5bcb7b9a70ef320cc91e7d85055cad3d02115192
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-02-05 22:07:40 -0800

    Create drawing-eraser.js

---
+++ drawing-eraser.js
@@ -0,0 +1,49 @@
+function setup() { 
+  createCanvas(400, 400);
+  colorMode(HSB);
+  fill(255);
+} 
+
+var diameter = 10;
+
+function eraser() {
+  fill(255);
+  noStroke();
+  diameter = 55;
+}
+  
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'R') {
+    fill(30, 100, 100);
+  } else if (key == 'G') {
+    fill(150, 150, 75);
+  } else if (key == 'B') {
+    fill(210, 50, 255); 
+  }
+  	else if (key == 'P') {
+    fill(330,150,100);
+  }
+  	else if (key == 'D') {
+    fill(240,100,100);
+  }
+  	else if (key == 'M') {
+    fill(270,50,200);
+  }
+  	else if (key == '1') {
+    diameter = 5;
+  }
+  	else if (key == '2') {
+    diameter = 15;
+  }
+  	else if (key == '3') {
+    eraser();
+  }
+}
